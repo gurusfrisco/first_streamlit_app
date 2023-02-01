6c0507624ed4f205c48e95c3017ef38cccb91b7e
gurusfrisco <123512688+gurusfrisco@users.noreply.github.com>
2023-01-31 21:22:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select color_or_style from catalog_for_website")
+my_cur.execute("select color_or_style from zenas_athleisure_db.products.catalog_for_website")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
